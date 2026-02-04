04a66bbcab1fde525cb41aa91e1e9b2a2585dc0b
Dan Valcour <newjourneyman@yahoo.com>
2026-02-04 12:42:00 -0500

    Update matrix example
    Use two fonts
    Use half font for text
    add some controls
    improve window resizing behaviour
    add ability to toggle fullscreen

--- examples/backend_glfw_opengl33.c
+++ examples/backend_glfw_opengl33.c
@@ -6,17 +6,23 @@ int main() {
   if (ctx == NULL) {
     return 1;
   }
-  const char *font_images[] = {"spartan_16x16_cp437.png"};
-  CTUI_Font *font =
-      CTUI_createFont("spartan_16x16_cp437.ctuifont", font_images, 1);
-  if (font == NULL) {
+  const char *cp437_16x16_images[] = {"cp437_16x16.png"};
+  CTUI_Font *font_16x16 =
+      CTUI_createFont("cp437_16x16.ctuifont", cp437_16x16_images, 1);
+  if (font_16x16 == NULL) {
+    return 2;
+  }
+  const char *cp437_8x16_images[] = {"cp437_8x16.png"};
+  CTUI_Font *font_8x16 =
+      CTUI_createFont("cp437_8x16.ctuifont", cp437_8x16_images, 1);
+  if (font_8x16 == NULL) {
     return 2;
   }
   const CTUI_DVector2 tile_pixel_wh = {16, 16};
   const size_t layer_count = 2;
   const CTUI_LayerInfo infos[2] = {
-      (CTUI_LayerInfo){.font = font, .tile_div_wh = {1, 1}},
-      (CTUI_LayerInfo){.font = font, .tile_div_wh = {1, 1}}};
+      (CTUI_LayerInfo){.font = font_16x16, .tile_div_wh = {1, 1}},
+      (CTUI_LayerInfo){.font = font_8x16, .tile_div_wh = {2, 1}}};
   const CTUI_ColorMode color = CTUIC_RGBA32;
   const char *title = "glfw opengl33 window";
   CTUI_Console *console = CTUI_createGlfwOpengl33FakeTerminal(
--- examples/frontend_matrix_rain.c
+++ examples/frontend_matrix_rain.c
@@ -32,6 +32,8 @@ void cleanup(CTUI_Context *ctx, CTUI_Console *console) {
   }
 }
 
+static int ANIMATION_RUNNING = 1;
+
 int runMainLoopBody(CTUI_Context *ctx, CTUI_Console *console) {
   CTUI_pollEvents(ctx);
   CTUI_Event event;
@@ -43,26 +45,39 @@ int runMainLoopBody(CTUI_Context *ctx, CTUI_Console *console) {
       if (event.data.key.key == CTUIK_ESCAPE &&
           event.data.key.action == CTUIA_PRESS) {
         return 0;
+      } else if (event.data.key.key == CTUIK_SPACE &&
+                 event.data.key.action == CTUIA_PRESS) {
+        ANIMATION_RUNNING = !ANIMATION_RUNNING;
+      } else if (event.data.key.key == CTUIK_F &&
+                 event.data.key.action == CTUIA_PRESS) {
+        if (CTUI_getIsFullscreen(console)) {
+          CTUI_setWindowedTileWh(console, (CTUI_SVector2){100, 100});
+        } else {
+          CTUI_setWindowedFullscreen(console);
+        }
       }
       break;
     case CTUI_EVENT_RESIZE: {
       CTUI_fitViewportTileWhToWindowPixelWh(console);
       const CTUI_SVector2 console_tile_wh = CTUI_getConsoleTileWh(console);
-      TRAIL_COUNT = console_tile_wh.x;
-      if (TRAIL_COUNT >= TRAIL_CAPACITY) {
+      const size_t new_trail_count = console_tile_wh.x;
+      if (new_trail_count > TRAIL_CAPACITY) {
         Trail *new_trails =
-            (Trail *)realloc(TRAILS, TRAIL_COUNT * sizeof(Trail));
+            (Trail *)realloc(TRAILS, new_trail_count * sizeof(Trail));
         if (new_trails == NULL) {
           return 0;
         }
         TRAILS = new_trails;
-        if (TRAIL_COUNT > TRAIL_CAPACITY) {
-          for (size_t i = TRAIL_CAPACITY; i < TRAIL_COUNT; i++) {
-            TRAILS[i] = (Trail){0};
-          }
+        TRAIL_CAPACITY = new_trail_count;
+        for (size_t i = TRAIL_COUNT; i < new_trail_count; i++) {
+          TRAILS[i] = (Trail){0};
         }
-        TRAIL_CAPACITY = TRAIL_COUNT;
+      } else if (new_trail_count < TRAIL_CAPACITY) {
+        for (size_t i = TRAIL_COUNT; i < TRAIL_CAPACITY; i++) {
+          TRAILS[i] = (Trail){0};
+        }
       }
+      TRAIL_COUNT = new_trail_count;
     } break;
     default:
       break;
@@ -71,29 +86,36 @@ int runMainLoopBody(CTUI_Context *ctx, CTUI_Console *console) {
   const CTUI_SVector2 console_tile_wh = CTUI_getConsoleTileWh(console);
   const int screen_height = console_tile_wh.y;
   const int screen_width = console_tile_wh.x;
-  CTUI_clear(console);
-  for (size_t i = 0; i < TRAIL_COUNT && i < screen_width; i++) {
-    Trail *trail = &TRAILS[i];
-    if (!trail->alive) {
-      if (rand() % 50 == 0) {
-        trail->alive = 1;
-        trail->x = i;
-        trail->head = 0;
-        trail->length = 5 + rand() % 15; // 5-19
-      }
-    } else {
-      trail->head++;
-      if (trail->head - trail->length >= screen_height) {
-        trail->alive = 0;
+  if (ANIMATION_RUNNING) {
+    for (size_t i = 0; i < TRAIL_COUNT; i++) {
+      Trail *trail = &TRAILS[i];
+      if (!trail->alive) {
+        if (rand() % 50 == 0) {
+          trail->alive = 1;
+          trail->x = i;
+          trail->head = 0;
+          trail->length = 5 + rand() % 50; 
+        }
+      } else {
+        trail->head++;
+        if (trail->head - trail->length >= screen_height) {
+          trail->alive = 0;
+        }
       }
     }
   }
-  CTUI_ConsoleLayer *layer = CTUI_getConsoleLayer(console, 0);
-  if (layer == NULL) {
-    CTUI_refresh(console);
-    return 1;
-  }
-  for (size_t i = 0; i < TRAIL_COUNT && i < screen_width; i++) {
+  CTUI_ConsoleLayer *layer0 = CTUI_getConsoleLayer(console, 0);
+  CTUI_ConsoleLayer *layer1 = CTUI_getConsoleLayer(console, 1);
+  CTUI_pushCstr(layer1, "Hello, and welcome to CTUI!", (CTUI_IVector2){1, 1},
+                99, 0, CTUI_BRIGHT_RED, CTUI_BLACK);
+  CTUI_pushCstr(layer1, "Press spacebar to start and stop time.",
+                (CTUI_IVector2){1, 4}, 99, 0, CTUI_BRIGHT_RED, CTUI_BLACK);
+  CTUI_pushCstr(layer1, "Press f to toggle fullscreen.", (CTUI_IVector2){1, 6},
+                99, 0, CTUI_BRIGHT_RED, CTUI_BLACK);
+  CTUI_pushCstr(layer1,
+                "Notice how this text uses a seperate font that is half width.",
+                (CTUI_IVector2){1, 9}, 99, 0, CTUI_BRIGHT_RED, CTUI_BLACK);
+  for (size_t i = 0; i < TRAIL_COUNT; i++) {
     Trail *trail = &TRAILS[i];
     if (trail->alive == 0) {
       continue;
@@ -105,11 +127,12 @@ int runMainLoopBody(CTUI_Context *ctx, CTUI_Console *console) {
         CTUI_Color fg_color;
         float intensity = 1.0f - (float)j / trail->length;
         fg_color = CTUI_RGBA32N(0, intensity, 0, 1);
-        CTUI_pushCodepoint(layer, (uint32_t)digit, (CTUI_IVector2){trail->x, y},
-                           fg_color, CTUI_BLACK);
+        CTUI_pushCodepoint(layer0, (uint32_t)digit,
+                           (CTUI_IVector2){trail->x, y}, fg_color, CTUI_BLACK);
       }
     }
   }
   CTUI_refresh(console);
+  CTUI_clear(console);
   return 1;
 }
